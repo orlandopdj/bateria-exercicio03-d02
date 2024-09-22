fdc7f499de4a4f814997e72cf3a537f227104d71
orlandopdj <orlandopdjr6@gmail.com>
2024-09-22 11:28:34 -0300

    ✅ criado componente DateMsg

--- src/app.jsx
+++ src/app.jsx
@@ -10,15 +10,27 @@ const App = () => {
     const incrementSteps = () => setStep((s) => s + 1)
     const incrementCount = () => setCount((c) => c + step)
 
-    const date = new Date()
-    const formattedDate = Intl.DateTimeFormat("pt-BR", {
-        weekday: 'long',
-        month: 'short',
-        year: 'numeric',
-        day: '2-digit'
-    }).format(date.setDate(date.getDate() + count))
+    const DateMsg = ({ count }) => {
+        const date = new Date()
+        const formattedDate = Intl.DateTimeFormat("pt-BR", {
+            weekday: 'long',
+            month: 'short',
+            year: 'numeric',
+            day: '2-digit'
+        }).format(date.setDate(date.getDate() + count))
 
-    const singularPlural = count === 1 || count === -1 ? 'dia' : 'dias'
+        const singularPlural = count === 1 || count === -1 ? 'dia' : 'dias'
+
+        return (
+            <h2>
+                {count > 0
+                    ? `${count} ${singularPlural} à partir de hoje será ${formattedDate}`
+                    : count < 0
+                        ? `${Math.abs(count)} ${singularPlural} atrás era ${formattedDate}`
+                        : `Hoje é ${formattedDate}`}
+            </h2>
+        )
+    }
 
     return (
         <>
@@ -28,20 +40,14 @@ const App = () => {
                     <h2>Intervalo: {step}</h2>
                     <button onClick={incrementSteps}>+</button>
                 </div>
-                
+
                 <div className="count">
                     <button onClick={decrementCount}>-</button>
                     <h2>Contagem: {count}</h2>
                     <button onClick={incrementCount}>+</button>
                 </div>
 
-                <h2>
-                    {count > 0
-                        ? `${count} ${singularPlural} à partir de hoje será ${formattedDate}`
-                        : count < 0
-                            ? `${Math.abs(count)} ${singularPlural} atrás era ${formattedDate}`
-                            : `Hoje é ${formattedDate}`}
-                </h2>
+                <DateMsg count={count} />
             </div>
         </>
     )
